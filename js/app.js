91f5f77f7f0ceac0357c8163a90486245c0dd7a2
afflz <augustovirtual317@gmail.com>
2016-10-12 17:29:22 -0300

    visual - done

---
+++ js/app.js
@@ -0,0 +1,76 @@
+
+// create the module and name it pokedapp
+var pokedapp = angular.module("pokedapp", ["ngRoute"]);
+
+// configure our routes
+pokedapp.config(function($routeProvider) {
+	$routeProvider.when("/", {
+		templateUrl : "templates/home.html",
+		controller  : "mainController",
+	})
+
+	// route for the  page
+	.when("/pokemon", {
+		templateUrl : "templates/pokemon.html",
+		controller  : "pokemonController",
+	})
+	.otherwise(
+		{
+			redirectTo: "/"
+		}
+	);
+});
+/**
+ * Serviço para manipulação dos objetos do serviço
+ */
+
+ var adrs = {};
+ var adrs.pokeapi = "http://pokeapi.co/api/v2/";
+
+app.factory('Service', function($http) {
+  var service = {};
+
+  /**
+   *  Função para tratar GET no serviço
+   */
+  service.get = function(url, callback) {
+    $http.get(url).then(function(response) {
+      var answer = response.data;
+      callback(answer);
+    });
+  };
+
+
+  /**
+   *  Função para tratar POST no serviço
+   */
+  service.post = function(url, data, callback) {
+    $http.post(url, data).then(function(response) {
+      var answer = response.data;
+      callback(answer);
+    });
+  };
+  /**
+   *  Função para tratar POST no serviço
+   */
+  service.delete = function(url, data, callback) {
+    $http.delete(url).then(function(response) {
+      var answer = response.data;
+    });
+  };
+
+  return service;
+});
+
+
+
+
+// create the controller and inject Angular"s $scope
+pokedapp.controller("mainController", function($scope) {
+	service.get
+
+});
+
+pokedapp.controller("pokemonController", function($scope) {
+
+});
